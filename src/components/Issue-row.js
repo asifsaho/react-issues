0db9d02a4a07f02e09d125b859daf8774b65f410
Asif Nawaz <asif.saho@gmail.com>
2018-12-15 16:24:44 +0100

    issue row missing templating

--- src/components/Issue-row.js
+++ src/components/Issue-row.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import helpers from '../helpers';
 
 const IssueRow = (props) => {
     return(
@@ -11,13 +12,13 @@ const IssueRow = (props) => {
                 <div className="issue-details">
                     <div className="issue-title">{props.issue.title}</div>
                     <div className="issue-meta-info">
-                        <small><span>ID:</span> #14443</small>
-                        <small><span>Created at:</span> Dec 14 2018 19:28</small>
+                        <small><span>ID:</span> #{props.issue.id}</small>
+                        <small><span>Created at:</span> {helpers.formatDate(props.issue.created_at)}</small>
                     </div>
                 </div>
 
                 <div className="user">
-                    <img src={props.issue.user.avatar_url}/>
+                    <img src={props.issue.user.avatar_url} alt={props.issue.user.name}/>
                 </div>
             </div>
         </div>
