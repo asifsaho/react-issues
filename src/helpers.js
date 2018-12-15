e3a16cc48bbb6e611bee2080da2e1bfa6a845d5a
Asif Nawaz <asif.saho@gmail.com>
2018-12-15 16:21:11 +0100

    formatDate function developed

---
+++ src/helpers.js
@@ -0,0 +1,16 @@
+function formatDate(isoDate) {
+    if ((new Date(isoDate)).toString() === 'Invalid Date') {
+        return;
+    }
+
+    const date = new Date(isoDate);
+    const day = (date.getDate() < 10) ? '0' + date.getDate() : date.getDate();
+    const month = (date.getMonth() < 9) ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+    const year = date.getFullYear();
+    return day + '-' + month + '-' + year
+}
+
+
+export default {
+    formatDate: formatDate
+};
